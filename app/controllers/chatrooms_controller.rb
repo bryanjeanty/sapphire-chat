8534d3180bf1a23f17fb302662472d9f1f6e3ca6
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-02-26 11:29:20 -0500

    create path to update chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,19 +1,26 @@
 class ChatroomsController < ApplicationController
-    # :index, :create, :show, :update, :destroy
     before_action :find_chatroom, only: [:show, :update, :destroy]
     def index
+        @chatrooms = Chatroom.all
+        @chatroom = Chatroom.new
     end
 
     def create
+        @chatroom = Chatroom.create(chatrooms_params)
+        redirect_to chatrooms_path
     end
 
     def show
     end
 
     def update
+        @chatroom.update(chatrooms_params)
+        redirect_to chatrooms_path
     end
 
     def destroy
+        @chatroom.destroy
+        redirect_to chatrooms_path
     end
 
     private
